17c5b1732b63c7c14d77e9e866198a2dd34d2a52
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-10-14 13:55:35 -0400

    First Commit

---
+++ lab07.c
@@ -0,0 +1,83 @@
+//HOMEWORK03 by RUI CHEN fb4138
+
+#include <pthread.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <semaphore.h> 
+//declare globe var, such as the three matrices and mutex lock
+int matrix1[256][256], matrix2[256][256], result[256][256];
+
+//to declare semaphore
+sem_t mutex;
+
+//this func is to write a matrix into a file called "output.txt"
+int matrixOutput(int matrix[256][256]){
+	FILE *fp=fopen("output.txt", "w+");
+	if(fp==NULL){
+		printf("could not open file!\n");
+		exit(-1);
+	}
+	
+	for(int i=0; i<256; i++){
+		for(int j=0; j<256; j++){
+			fprintf(fp,"%d ", matrix[i][j]);	
+		}
+		fprintf(fp,"\n");
+	}
+	fclose(fp);
+}
+
+
+void* matrixAdd(void *i){		
+	//first need to convert i datatype
+	int num=(int)i;
+
+	//put a semaphore so only one thread can modify the result matrix at once
+	________
+
+	//this thread will do addition for rows from num*32 to num*32+31
+	???
+	
+	printf("Thread %d Done!\n", num);
+	
+	//to release the semaphore
+	________
+
+	pthread_exit(NULL);
+}
+
+
+int main(){
+	//to initialize semaphore
+	________
+
+	//firstly initialize the two matrices
+	//both matrix 1 and 2 will be like:
+	// 1,2,...,256
+	// 1,2,...,256
+	// ...
+	// 1,2,...,256
+	for(int i=0; i<256; i++){
+		for(int j=0; j<256; j++){
+			matrix1[i][j]=j+1;
+			matrix2[i][j]=j+1;
+			result[i][j]=0;
+		}
+	}
+
+	//secondly to create the threads and assign jobs for them
+	pthread_t thread[8];	
+	for(int i=7; i>=0; i--) { 
+		________
+	}
+
+	//thirdly use pthread join to make sure all threads are done before the main thread
+	for(int i=7; i>=0; i--) { 
+		_________
+	}
+	
+	//lastly to write the result into output.txt
+	matrixOutput(result);
+	
+	return 0;
+}
